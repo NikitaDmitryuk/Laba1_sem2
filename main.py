bc3fa04050e934fad9af22188d01b31d3fdbb885
NikitaDmitryuk <dmitryuk.nikita@gmail.com>
2020-03-26 21:02:24 +0300

    Мелкие правки

--- main.py
+++ main.py
@@ -38,6 +38,8 @@ def plot_trajectory(modeling, detector=None):
         trajectory = photon.get_trajectory()
         ax.plot(trajectory[0], trajectory[1], trajectory[2], '--.')
 
+    plt.savefig('trajectory_{:.1e}_{}.pdf'.format(n, surface_height), bbox_inches='tight')
+
 
 @timer
 def plot_hist(detector):
@@ -55,9 +57,10 @@ def plot_hist(detector):
     position = detector.get_position()
     plt.suptitle('Шайба высотой {}, координаты детектора ({}, {}, {}), количество частиц {:.1e}'
                  .format(position[2], position[0], position[1], position[2], n))
+    plt.savefig('plot_hist_{:.1e}_{}.pdf'.format(n, surface_height), bbox_inches='tight')
 
 
-n = 100000
+n = 1000
 start_energy = 3.5
 surface_radius = 20
 surface_height = 60
@@ -67,15 +70,14 @@ n_bins_hist = 20
 
 
 def main():
-    start_time = time.process_time()
 
+    start_time = time.process_time()
     surface = CylinderSurface(surface_radius, surface_height)
     source = RectangularSource(source_height, source_width)
     modeling = Modeling(surface, source, n, start_energy)
     detector = Detector(surface_height)
 
     modeling.start_of_modeling()
-
     detector.flow_rate(modeling, n_bins_hist)
 
     print("FULL MODELING TIME: {:g} s".format(time.process_time() - start_time))
@@ -84,7 +86,6 @@ def main():
         plot_trajectory(modeling, detector)
 
     plot_hist(detector)
-
     plt.show()
 
 
